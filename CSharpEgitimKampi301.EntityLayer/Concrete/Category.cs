b08ca69b4caf129d0e443c0de3839ddbfe34926b
tahir ekrem koru <tahirekremkoru@gmail.com>
2024-11-02 00:16:43 +0300

    Katmanlı Mimari Giriş

--- CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace CSharpEgitimKampi301.EntityLayer.Concrete
 {
-    internal class Category
+    public class Category
     {
+        public int CategoryId { get; set; }
+        
+        public string CategoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
     }
 }
+/*
+Field-Variable-Property
+ 
+int x --> Field (değişken yapısı bir sınıfın içine tanımlanırsa field olur.)
+int y --> Variable (değişken yapısı bir metodun içinde tanımlanırsa variable olur.)
+int z {get;set;} --> Property (değişken yapısı sonuna get set ifadesi alırsa property olur.)
+ */
